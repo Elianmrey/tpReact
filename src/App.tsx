52e8f0cdfed4a4ff93e90422dfbc74c1c0aeb20f
Elianmrey <elian060893@gmail.com>
2024-11-03 11:23:49 -0300

    Rotas ajustadas para melhor semantica

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { RouterProvider } from "react-router-dom"
-import routes  from "./Routes/index.tsx"
+import routes from "./Routes/Index.tsx"
 import "./App.scss";
 function App() {
-  
+
   return (
     <RouterProvider router={routes} />
   )
--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import Home from "../Views/Home/Index.tsx";
-import Protected from "./Protected";
+import Protected from "./Protected.tsx";
 import { HandleVerificationProtected, IsAuthenticated } from "../Services/Authentication.tsx";
 import Settings from "../Views/Settings/Index.tsx";
 import SignUp from "../Views/SignUp/Index.tsx";
@@ -9,25 +9,19 @@ import Dashboard from "../Views/Dashboard/Index.tsx";
 import Notfound from "../Views/NotFound/Index.tsx";
 import Formulary from "../Views/Formulary/Index.tsx";
 
-
-
-
-
-
 const routes = createBrowserRouter(
     createRoutesFromElements(
-        //Protected é uma rota protegida
-        <Route path="/" element={<Protected />} >
-            <Route>
+
+        <Route path="/"  >
+            <Route element={<Protected />}>
                 <Route path="/" index element={<Home />} loader={() => HandleVerificationProtected()} />
                 <Route path="/settings" element={<Settings />} loader={() => HandleVerificationProtected()} />
                 <Route path="/dashboard" element={<Dashboard />} loader={() => HandleVerificationProtected()} />
                 <Route path="/formulary" element={<Formulary />} loader={() => HandleVerificationProtected()} />
                 <Route path="/*" element={<Notfound />} loader={() => HandleVerificationProtected()} />
-
             </Route>
 
-            <Route path="/signin" element={<SignIn />} loader={() => IsAuthenticated()}  />
+            <Route path="/signin" element={<SignIn />} loader={() => IsAuthenticated()} />
             <Route path="/signup" element={<SignUp />} loader={() => IsAuthenticated()} />
 
 
