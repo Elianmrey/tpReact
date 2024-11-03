4604850f63e1ef9f7569aaea3950753b63b80694
Elianmrey <elian060893@gmail.com>
2024-11-03 13:05:09 -0300

    Mudanças de Nome para evitar conflitos de rotaas internas do projeto

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
-import Home from "../Views/Home/Index.tsx";
+import Home from "../Views/Home/Home.tsx";
 import Protected from "./Protected/Protected.tsx";
 import { HandleVerificationProtected, IsAuthenticated } from "../Services/Authentication.tsx";
-import Settings from "../Views/Settings/Index.tsx";
-import SignUp from "../Views/SignUp/Index.tsx";
-import SignIn from "../Views/SignIn/Index.tsx";
-import Dashboard from "../Views/Dashboard/Index.tsx";
-import Notfound from "../Views/NotFound/Index.tsx";
-import Formulary from "../Views/Formulary/Index.tsx";
+import Settings from "../Views/Settings/Settings.tsx";
+import SignUp from "../Views/SignUp/SignUp.tsx";
+import SignIn from "../Views/SignIn/SignIn.tsx";
+import Dashboard from "../Views/Dashboard/Dashboard.tsx";
+import Notfound from "../Views/NotFound/NotFound.tsx";
+import Formulary from "../Views/Formulary/Formulary.tsx";
 
 const routes = createBrowserRouter(
     createRoutesFromElements(
