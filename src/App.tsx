bb1c482247a657cee1436a53d9739303fe97a0ff
Elianmrey <elian060893@gmail.com>
2024-11-03 18:48:34 -0300

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { RouterProvider } from "react-router-dom"
+import { RouterProvider } from "react-router-dom";
 import routes from "./Routes/Index.tsx";
 import "./App.scss";
 function App() {
